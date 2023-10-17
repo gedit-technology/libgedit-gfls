d18596ea8c5b1da2e39d098f1258b43c4b615fb2
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2023-10-17 16:00:00 +0200

    gui: add an Open File button
    Still does nothing when clicked.

--- src/gfls-gui.c
+++ src/gfls-gui.c
@@ -10,18 +10,30 @@ activate_cb (GApplication *g_app,
 	     gpointer      user_data)
 {
 	GtkWidget *window;
+	GtkGrid *hgrid;
+	GtkButton *open_file_button;
 	GtkTextView *view;
 	GtkWidget *scrolled_window;
 
 	window = gtk_application_window_new (GTK_APPLICATION (g_app));
 	gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
 
+	hgrid = GTK_GRID (gtk_grid_new ());
+
+	open_file_button = GTK_BUTTON (gtk_button_new_with_label ("Open File"));
+	gtk_widget_set_valign (GTK_WIDGET (open_file_button), GTK_ALIGN_START);
+	gtk_container_add (GTK_CONTAINER (hgrid), GTK_WIDGET (open_file_button));
+
 	view = GTK_TEXT_VIEW (gtk_text_view_new ());
 	gtk_text_view_set_monospace (view, TRUE);
+	g_object_set (view,
+		      "expand", TRUE,
+		      NULL);
 
 	scrolled_window = gtk_scrolled_window_new (NULL, NULL);
 	gtk_container_add (GTK_CONTAINER (scrolled_window), GTK_WIDGET (view));
-	gtk_container_add (GTK_CONTAINER (window), scrolled_window);
+	gtk_container_add (GTK_CONTAINER (hgrid), scrolled_window);
+	gtk_container_add (GTK_CONTAINER (window), GTK_WIDGET (hgrid));
 
 	gtk_widget_show_all (window);
 }
