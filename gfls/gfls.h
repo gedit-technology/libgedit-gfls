11eaaab595c092e7db97c121c087ee19b7bb37ff
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2023-10-17 16:00:00 +0200

    Add empty main header

---
+++ gfls/gfls.h
@@ -0,0 +1,12 @@
+/* SPDX-FileCopyrightText: 2023 - Sébastien Wilmet
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+#ifndef GFLS_H
+#define GFLS_H
+
+#define GFLS_H_INSIDE
+
+#undef GFLS_H_INSIDE
+
+#endif /* GFLS_H */
