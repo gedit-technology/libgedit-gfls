78da9278f2d17ee2a99efab061a89f763d488eaa
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2023-10-31 16:00:00 +0100

    utf8: has_very_long_line(): write some unit tests
    Looks good enough.

--- tests/unit-tests/test-utf8.c
+++ tests/unit-tests/test-utf8.c
@@ -7,7 +7,42 @@
 static void
 test_has_very_long_line (void)
 {
-	/* TODO */
+	/* Without newline chars */
+	g_assert_false (gfls_utf8_has_very_long_line ("", 0));
+	g_assert_true (gfls_utf8_has_very_long_line ("a", 0));
+
+	g_assert_false (gfls_utf8_has_very_long_line ("", 1));
+	g_assert_false (gfls_utf8_has_very_long_line ("a", 1));
+	g_assert_true (gfls_utf8_has_very_long_line ("ab", 1));
+
+	g_assert_true (gfls_utf8_has_very_long_line ("é", 1));
+	g_assert_false (gfls_utf8_has_very_long_line ("é", 2));
+
+	/* With \n */
+	g_assert_false (gfls_utf8_has_very_long_line ("\n\n\n", 0));
+	g_assert_true (gfls_utf8_has_very_long_line ("\na\n", 0));
+	g_assert_true (gfls_utf8_has_very_long_line ("\na", 0));
+
+	g_assert_false (gfls_utf8_has_very_long_line ("a\nb\nc\n", 1));
+	g_assert_false (gfls_utf8_has_very_long_line ("a\nb\nc", 1));
+	g_assert_true (gfls_utf8_has_very_long_line ("a\nbc\n", 1));
+	g_assert_true (gfls_utf8_has_very_long_line ("a\nbcd\n", 1));
+	g_assert_true (gfls_utf8_has_very_long_line ("a\nbcd\n", 2));
+	g_assert_true (gfls_utf8_has_very_long_line ("a\nbcd", 2));
+
+	g_assert_true (gfls_utf8_has_very_long_line ("é\nSé", 2));
+	g_assert_false (gfls_utf8_has_very_long_line ("é\nSé", 3));
+	g_assert_false (gfls_utf8_has_very_long_line ("é\nSé\n", 3));
+
+	/* Quick test with \r */
+	g_assert_false (gfls_utf8_has_very_long_line ("\r\r\r", 0));
+	g_assert_false (gfls_utf8_has_very_long_line ("\n\r\n\r\n\r", 0));
+	g_assert_false (gfls_utf8_has_very_long_line ("\r\n\r\n\r\n", 0));
+
+	g_assert_false (gfls_utf8_has_very_long_line ("a\rb\nc\rd", 1));
+
+	g_assert_true (gfls_utf8_has_very_long_line ("a\ré", 1));
+	g_assert_false (gfls_utf8_has_very_long_line ("a\ré", 2));
 }
 
 int
